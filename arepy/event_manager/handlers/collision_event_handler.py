34b5e44611e51f17ab2a598a8786e20bc622f899
Abrahan <scr44gr@gmail.com>
2024-01-28 06:34:15 -0400

    fix: collision event handler

--- arepy/event_manager/handlers/collision_event_handler.py
+++ arepy/event_manager/handlers/collision_event_handler.py
@@ -8,25 +8,25 @@ class CollisionEventHandler:
     """A class that stores the collision handler function and the entities that it handles"""
 
     def __init__(self):
-        self.current_collisions: Dict[Entity, Entity] = {}
+        self.current_collisions: Dict[int, Entity] = {}
 
     def drop_collision(self, entity: Entity):
         """Remove the entity from the current collisions"""
-        if entity in self.current_collisions:
-            self.current_collisions.pop(entity)
+        if entity.get_id() in self.current_collisions:
+            self.current_collisions.pop(entity.get_id())
 
     def add_collision(self, entity_a: Entity, entity_b: Entity):
         """Add the collision to the current collisions"""
-        self.current_collisions[entity_a] = entity_b
-        self.current_collisions[entity_b] = entity_a
+        self.current_collisions[entity_a.get_id()] = entity_b
+        self.current_collisions[entity_b.get_id()] = entity_a
 
     def is_colliding(self, entity: Entity) -> bool:
         """Return True if the entity is colliding with another entity"""
-        return entity in self.current_collisions
+        return entity.get_id() in self.current_collisions
 
     def get_colliding_entity(self, entity: Entity) -> Entity:
         """Return the entity that the entity is colliding with"""
-        return self.current_collisions[entity]
+        return self.current_collisions[entity.get_id()]
 
     def on_collision(self, event: CollisionEvent):
         """Collision handler function"""
