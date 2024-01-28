eafd9853a90f22bee3d929ef14542b2366c86e55
Abrahan <scr44gr@gmail.com>
2024-01-27 20:58:44 -0400

    chore!: add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = "0.1.0"
+DESCRIPTION = "An ECS game engine library created with SDL2 and Python."
+LONG_DESCRIPTION = long_description
+
+# Setting up
+
+setup(
+    name="arepy",
+    version=VERSION,
+    author="Scr44gr",
+    author_email="<scr44gr@protonmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["PyGLM", "pysdl2", "pysdl2dll"],
+    keywords=["ecs", "game-engine", "sdl2-wrapper", "python-game-engine"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
