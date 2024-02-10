5d8e40a889fbe2bc936d7f8680a1ccf3c77b5270
Abrahan <scr44gr@gmail.com>
2024-02-10 10:43:37 -0400

    fix: readme.md decoding to utf-8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_bytes().decode("utf-8")
 
 VERSION = "0.1.1"
 DESCRIPTION = "An ECS game engine library created with SDL2 and Python."
